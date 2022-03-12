2f82abeec05c7a8c6c0dfe0945c3b3e13ca37643
daeltonpinheiro <danben84@gmail.com>
2022-03-12 15:15:12 +0000

    commit

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,16 +3,19 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    public List<Topico> lista(){
+    public List<TopicoDto> lista(){
  
         Topico topico1 = new Topico("Dúvida","Dúvidas com Spring",new Curso("Spring","Programacao Back-End - Java"));
         Topico topico2 = new Topico("Dúvida","Dúvidas com Oracle",new Curso("Oracle","Programacao Back-End - Oracle"));
