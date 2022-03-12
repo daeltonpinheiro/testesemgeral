1e38f52235082cc5459d9c57356efe37b98e4341
daeltonpinheiro <danben84@gmail.com>
2022-03-12 23:35:20 +0000

    commit

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
