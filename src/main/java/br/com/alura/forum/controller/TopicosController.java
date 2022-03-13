0f369ec4b90d49a2b409d52cc4e1956d613a7b5c
daeltonpinheiro <danben84@gmail.com>
2022-03-13 01:01:48 +0000

    commit

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -17,11 +17,16 @@ public class TopicosController {
     private TopicoRepository topicoRepository;
 
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista(){
- 
-        List<Topico> topicos = topicoRepository.findAll();
+    public List<TopicoDto> lista(String nomeCurso){
 
-        return TopicoDto.converter(topicos);
+        if(nomeCurso == null){
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        }else{
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
+        
     }
     
 }
