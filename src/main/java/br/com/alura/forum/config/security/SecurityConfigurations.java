dd9f8063e0881a99521c5899f0c06a027e99f4fe
daeltonpinheiro <danben84@gmail.com>
2022-03-23 12:06:05 +0000

    continue security

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +12,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
     
-    
+    @Override //configurações de autenticação
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        
+    }
+
+    @Override // configurações de recursos estáticos, tecnologias de view(jsp, js, css, img, etc)
+    public void configure(WebSecurity web) throws Exception {
+        
+    }
+
+    @Override //configurações de autorização
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers(HttpMethod.GET,"/topicos").permitAll()
+        .antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
+        .;
+    }
+
 }
