3e7fed2d064839182dff33243262934ce9473f05
daeltonpinheiro <danben84@gmail.com>
2022-03-23 12:57:37 +0000

    security continue

--- src/main/java/br/com/alura/forum/modelo/Usuario.java
+++ src/main/java/br/com/alura/forum/modelo/Usuario.java
@@ -1,12 +1,19 @@
 package br.com.alura.forum.modelo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails{
 
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +21,8 @@ public class Usuario {
 	private String nome;
 	private String email;
 	private String senha;
+
+	private List<Perfil> perfis = new ArrayList<>();
 
 	@Override
 	public int hashCode() {
@@ -70,6 +79,48 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return this.perfis;
+	}
+
+	@Override
+	public String getPassword() {
+		
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		
+		return true;
 	}
 
 }
